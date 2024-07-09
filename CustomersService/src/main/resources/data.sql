9685b78785c663ef107650a924cca52f6e928608
Vikram Shanbogar <70604290+vikramshanbogar@users.noreply.github.com>
2024-07-09 11:53:50 +0530

    delete db scripts

--- CustomersService/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `customer`(f_name,l_name) VALUES
-                                          ('Vikram','Shanbogar'),
-                                          ('Pooja','Raj'),
-                                          ('Swati','Kota'),
-                                          ('Varun','Tej');
